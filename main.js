b681c8023ad3546eb3225edba40b4082f4b42df6
Valeriya Terebilova <yakontekst01@yandex.ru>
2025-06-30 16:19:36 +0300

    new file:   index.html         new file:   main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+(()=>{
+    const l = (log, str = ' ') => {
+        console.log(log, str);
+    }
+    l("hello")
+    /**
+     * https://todomvc.com/ Реализации приложения TODO на различных языках и фреймворках
+     */
+})()
